73fe221b3be2cd630ddeffb6e149d07d884199db
xiongfeng <cpla026@163.com>
2017-12-03 15:45:19 +0800

    Initial commit

---
+++ src/com/cpla/test/HelloWord.java
@@ -0,0 +1,10 @@
+package com.cpla.test;
+
+/**
+ * Created by Administrator on 2017/12/3.
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.println("helloWord");
+    }
+}
